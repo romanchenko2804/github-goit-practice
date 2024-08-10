59934f2b8709006446fcc80fb92c012b2ae3b524
Mykola Romanchenko <83551501+romanchenko2804@users.noreply.github.com>
2024-08-10 22:33:28 +0300

    +JS

--- js/script.js
+++ js/script.js
@@ -63,17 +63,54 @@
 
 //   console.log(formatMessage("This is my message.", 19))
 
-function checkForSpam(message) {
-    let result;
-    // Change code below this line
+// function checkForSpam(message) {
+//     let result;
+//     // Change code below this line
 
-      if (message.toLowerCase().includes("spam") ||message.toLowerCase().includes("sale") ) {
-        result = true;
-      } else  {
-        result = false;
-      }
-    // Change code above this line
-    return result;
+//       if (message.toLowerCase().includes("spam") ||message.toLowerCase().includes("sale") ) {
+//         result = true;
+//       } else  {
+//         result = false;
+//       }
+//     // Change code above this line
+//     return result;
+//   }
+
+//   console.log(checkForSpam('Get best Sale offers now!'))
+
+// function getGrade (s1, s2, s3) {
+//   // Code here
+//   let score = Math.round((s1+s2+s3)/3);
+//   ceonsole.log(score);
+//   let letter = '';
+
+//   if (90 <= score && score <= 100){
+//     letter = 'A';
+//   } else if (80 <= score && score < 90) {
+//     letter = 'B';
+//   } else if (70 <= score && score < 80) {
+//     letter = 'C';
+//   } else if (60 <= score && score < 70) {
+//     letter = 'D';
+//   } else if (0 <= score && score < 60) {
+//     letter = 'F';
+//   }
+//   console.log(letter);
+//   return letter;
+// }
+
+// getGrade (40, 60, 90)
+// // console.log(gtGrade (40, 60, 90));
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+  let love = false;
+  if ((flower1 + flower2) % 2 !== 0) {
+    love = true;
   }
+  return love;
+}
 
-  console.log(checkForSpam('Get best Sale offers now!'))
+console.log(lovefunc(3, 2))
+
+// console.log(typeof(true))
