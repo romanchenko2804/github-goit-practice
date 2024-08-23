fd9f55ce0a61872981338a47bcaf04bd0dea12d8
Mykola Romanchenko <83551501+romanchenko2804@users.noreply.github.com>
2024-08-23 11:47:20 +0300

    +js files

---
+++ js/script-3.js
@@ -0,0 +1,71 @@
+// ****???????????????????????      #1
+
+// function order(words){
+//     let array = [];
+//     let str = '';
+//     const ar = words.split(' ');
+//     console.log(ar);
+    
+// }
+
+// console.log(order("is2 Thi1s T4est 3a"));
+
+//   Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  
+// "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+// ******************
+
+//  *****************      #2
+
+// const removeSomeElem = function (str) {
+//     return str.split(/[-]/)
+
+// }
+
+// console.log(removeSomeElem('Mykola-run_Enjoy-nice'));
+
+//****************
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// const words = str.split(' ');
+// console.log(words);
+// // console.log(words[4]);
+
+// // 
+// const chars = str.split('');
+// // console.log(chars);
+// // console.log(chars[28]);
+
+
+// const strCopy = str.split();
+// // console.log(strCopy);
+
+
+// const index = 4;
+// // console.log(`The character at index ${index} is ${str.charAt(index)}`);
+
+
+
+// console.log((str.split(' '))[1]);
+// console.log(str.charAt(0));
+
+
+// *****************************
+
+
+function duplicateEncode(word){
+    let str = '';
+    const arr = word.split('');
+    console.log(arr);
+  
+      
+    return str;
+  }
+  
+  console.log(duplicateEncode('rece'));
+
+  
--- js/script.js
+++ js/script.js
@@ -224,4 +224,236 @@
 
 // console.log(grow([1,2,3,4]))
 
-// *****************
+//***********************
+// моє рішення:
+// function sumTwoSmallestNumbers(numbers) {
+//     let array = [];
+
+//     for (let i = 0; i < numbers.length; i += 1) {
+//         const num = numbers[i];
+//         const min = Math.min(...numbers);
+//         const index = numbers.indexOf(min);
+        
+//         numbers.splice(index, 1)
+        
+//         array.push(min);
+
+//     }
+
+
+//     return array[0]+array[1];
+//   }
+
+
+//COPILOT HELP #1:
+
+// function sumTwoSmallestNumbers(numbers) {
+//     const min1 = Math.min(...numbers);
+//     const index = numbers.indexOf(min1);
+//     numbers.splice(index, 1);
+//     const min2 = Math.min(...numbers);
+
+
+//     return min1+min2;
+//   }
+
+  //COPILOT HELP #2:
+
+//   function sumTwoSmallestNumbers(numbers) {
+//     // Сортуємо масив чисел у зростаючому порядку
+//     numbers.sort((a, b) => a - b);
+    
+//     // Повертаємо суму двох найменших чисел
+//     return numbers[0]+numbers[1];
+//   }
+
+//   console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));
+
+
+// ********************
+
+
+// function between(a, b) {
+//   //мої костилі ==>
+
+//   // let array = [];
+//   // let c = array.length;
+
+//   // c=b-a+1;
+
+//   // if (a<0) {
+//   //   c=-a+b+1;
+//   // }
+
+//   // for (let i = 0; i < c; i += 1) {
+//   //   const elem = array[i]; 
+
+//   //   if (array[c] !== b) {
+//   //     array.push(a+i)
+//   //   }
+//   // } 
+
+//   // чатік ==>
+
+//   let array = [];
+//     for (let i = a; i <= b; i++) {
+//         array.push(i);
+//     }
+
+//   return array;
+// }
+
+// console.log(between(-2, 2));
+
+// **********************
+
+// function solution(number){
+//   let multiply = 0;
+//   let arr = [];
+
+//   if (number < 0) {
+//     return 0;
+//   }
+
+//   for (let i = 0; i < number; i+=1) {
+//     arr.push(i)
+//   }
+  
+//   for (let i = 1; i < arr.length; i+=1) {
+//     const elem = arr[i];
+
+//     if (elem%3===0 || elem%5===0) {
+
+//       multiply+=elem
+//     }
+
+//   }
+  
+//   return `Результат виконання функції: ${multiply}`;
+// }
+
+// console.log(solution(10));
+
+// *************************************
+
+// const student = {
+//   name : "David Rayy",
+//   sclass : "VI",
+//   rollno : 12 };
+
+//   console.log(Object.keys(student));
+//   console.log(Object.values(student));
+
+// ************************
+
+//Print (console.log()) value of the property name in the given object:
+// let student = {
+//   name:"Max",
+//   age: 27
+// };
+// console.log (student.name);
+
+
+//Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, discount it by 7%. Update the object with :
+
+//the new property - discount and the corresponding value (7% or 10%) and the new price.
+
+// let product = {
+//   name: "headphones",
+//   price: 100,
+//   discount: 0
+// };
+
+// const discounts = product.price > 100 ? product.discount = 10 : product.discount = 7
+
+// product.price =product.price - (product.price*product.discount/100);
+// // if (product.price > 100) {
+// //   product.discount = 10;
+// // } else {
+// //   product.discount = 7;
+// // }
+
+// console.log(product);
+
+
+//Use given object and loop through its properties and if it has property “discount” print “Already discounted by…” and add the value (how much was the discount). In opposite case, do what you did in the previous exercise: check the price, depending on it (if it’s greater or lower than 100) add discount (10% or 7%) and add the property “discount” to the object.
+//Note: To test, un-comment out line discount: "7%".
+let product = {
+  name: "headphones",
+  price: 100,
+  discount: "7%",
+};
+
+// let message = `Already discounted by ${product.discount}`
+
+const discounts = product.price > 100 ? product.discount = 10 : product.discount = 7;
+
+product.price = product.price - (product.price*product.discount/100);
+
+for (const key in product) {
+    const elem = product[key];
+
+    if (key === 'discount') {
+      // return message;
+      console.log(product.discount)
+    }
+    
+}
+
+console.log(product);
+// console.log(Math.round(product.price));
+// end result - case 1:
+// { name: 'headphones', price: '77.84', discount: '7%' }
+
+// end result - case 2:
+// Already discounted by 7%.
+
+
+// *************************
+
+
+// // creating a simple CAR obj
+
+// const car = {
+//   make: 'Toyota',
+//   model: 'Corolla',
+//   year: 2021
+// };
+// // Displaying the car object
+// // console.log(car);
+// const textOfModel = 'model';
+
+// const carModel = car.model;
+// const carModel2 = car[textOfModel];
+// const carModel3 = car['model'];
+// console.log('Значення властивості через точку -',carModel);
+// console.log('Значення властивості через квадратні дужки -',carModel2);
+// console.log('Значення властивості через квадратні дужки, де властивість передана як РЯДОК -',carModel3);
+
+// car.year = 2022;
+// // console.log(car);
+
+// for (const key in car) {
+//     // const element = ;
+//     // console.log(key, ':', car[key]);
+// }
+
+// // console.log(Object.values(car));
+
+// // Creating an Array of Objects:
+// let cars = [
+//   { make: "Toyota", model: "Corolla", year: 2021 },
+//   { make: "Honda", model: "Civic", year: 2020 },
+//   { make: "Ford", model: "Focus", year: 2022 }
+// ];
+
+// // Creating a person object with a nested address object
+// let person = {
+//   name: "Alice Smith",
+//   age: 30,
+//   address: {
+//       street: "123 Main St",
+//       city: "Springfield",
+//       zipCode: "12345"
+//   }
+// };
